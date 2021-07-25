e02ff1df519f60356e871f53ead9f4265022b0be
Ahmet <ahmetayyildiz.dev@gmail.com>
2021-07-25 12:49:53 +0300

    Proje dosyası ekle.

---
+++ İnnertypeModelleme/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace İnnertypeÖdevProje
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ürün m1 = new Ürün();
+            Ürün.ekmek e1 = new Ürün.ekmek();
+            e1.Markalar = "pastek";
+            object m10 = new object();
+            m10 = 20;
+            Console.WriteLine(m10.GetType());
+            
+        }
+    }
+
+    class Ürün
+    {
+        public string Markalar { get; set; }
+        
+
+
+        public class ekmek : Ürün
+        {
+            public int agırılık { get; set; }
+            public string cesit { get; set; }
+            public string tür { get; set; }
+        }
+    }
+    
+
+}
+    
